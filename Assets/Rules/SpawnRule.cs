e51e68881f1251330d83987d0b0088368abf3856
Romano McBride <git@somian.net>
2024-01-28 15:40:04 -0500

    randomized spawn locations

--- Assets/Rules/SpawnRule.cs
+++ Assets/Rules/SpawnRule.cs
@@ -7,11 +7,16 @@ using UnityEngine;
 public class SpawnRule : AwesomeRule
 {
 	public GameObject objectToSpawn;
+	public float spawnAreaSize = 3.0f;
 
 	public override void Trigger()
 	{
 		if(objectToSpawn){
-			Instantiate(objectToSpawn);
+			float randX = Random.Range(-spawnAreaSize, spawnAreaSize);
+			float randY = Random.Range(-spawnAreaSize, spawnAreaSize);
+
+			Vector3 spawnPos = new Vector3(randX,0,randY);
+			Instantiate(objectToSpawn, spawnPos, Quaternion.identity);
 		}
 	}
 }
