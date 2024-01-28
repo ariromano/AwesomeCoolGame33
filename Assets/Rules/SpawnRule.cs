9b2b41d1760cc2b095b72af611a704205fabd1ab
Romano McBride <git@somian.net>
2024-01-28 15:45:00 -0500

    fixed lava color

--- Assets/Rules/SpawnRule.cs
+++ Assets/Rules/SpawnRule.cs
@@ -8,6 +8,8 @@ public class SpawnRule : AwesomeRule
 {
 	public GameObject objectToSpawn;
 	public float spawnAreaSize = 3.0f;
+	public float spawnHeight = 3.0f;
+
 
 	public override void Trigger()
 	{
@@ -15,7 +17,7 @@ public class SpawnRule : AwesomeRule
 			float randX = Random.Range(-spawnAreaSize, spawnAreaSize);
 			float randY = Random.Range(-spawnAreaSize, spawnAreaSize);
 
-			Vector3 spawnPos = new Vector3(randX,0,randY);
+			Vector3 spawnPos = new Vector3(randX,spawnHeight,randY);
 			Instantiate(objectToSpawn, spawnPos, Quaternion.identity);
 		}
 	}
